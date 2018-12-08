23bf6f5b78f1da910704f6d974e4122510679e57
MUHAMMED NABEEL M <nabeelin1994@gmail.com>
2018-12-09 00:37:32 +0530

    Add files via upload
    Assign the strings in the previous exercise to a variable and use the variable to print the string

---
+++ ass3.py
@@ -0,0 +1,15 @@
+
+#def request():
+a="Hello, World"
+b="Python is a wonderful language"
+c="I am beginner in Python"
+
+
+def printing():
+
+    print(a)
+    print(b)
+    print(c)
+
+#request()
+printing()
